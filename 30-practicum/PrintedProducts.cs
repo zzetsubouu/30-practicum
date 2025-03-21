ad1f2d0aa1f214f3dbdec70112909f66020ca029
Kab-35-16 <Kab-35-16@KAB-31-8->
2025-03-21 11:41:16 +0200

    2 задание

--- 30-practicum/PrintedProducts.cs
+++ 30-practicum/PrintedProducts.cs
@@ -9,7 +9,24 @@ namespace _30_practicum
     abstract class PrintedProducts
     {
         private string title;
-        public string Title { get => title; set => title = value; }
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                if (value == "")
+                {
+                    throw new Exception("Название не может быть пустым");
+                }
+            }
+        }
+        public PrintedProducts()
+        {
+
+        }
         public PrintedProducts(string title)
         {
             Title = title;
